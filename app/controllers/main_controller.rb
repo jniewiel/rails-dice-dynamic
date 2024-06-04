a9a9722b5812a003fec55239e4ecd36e5ee65690
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-04 19:39:27 +0000

    Made dice rolls flexible, taking any number of dice and rolls requested.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,60 +4,77 @@ class MainController < ApplicationController
     render({:template => "dice/home"})
   end
 
+  # # ------------------------------------------------------------------ #
+
+  # def two_six
+  #   @rolls = []
+
+  #   2.times do
+  #     die = rand(1..6)
+  
+  #     @rolls.push(die)
+  #   end
+
+  #   render({:template => "dice/two_six"})
+  # end
+
+  # # ------------------------------------------------------------------ #
+  
+  # def two_ten
+  #   @rolls = []
+
+  #   2.times do
+  #     die = rand(1..10)
+  
+  #     @rolls.push(die)
+  #   end
+
+  #   render({:template => "dice/two_ten"})
+  # end
+
+  # # ------------------------------------------------------------------ #
+
+  # def one_twenty
+  #   @rolls = []
+
+  #   1.times do
+  #     die = rand(1..20)
+  
+  #     @rolls.push(die)
+  #   end
+
+  #   render({:template => "dice/one_twenty"})
+  # end
+
+  # # ------------------------------------------------------------------ #
+
+  # def five_four
+  #   @rolls = []
+
+  #   5.times do
+  #     die = rand(1..4)
+  
+  #     @rolls.push(die)
+  #   end
+
+  #   render({:template => "dice/five_four"})
+  # end
+
   # ------------------------------------------------------------------ #
 
-  def two_six
+  def flexible
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+    
     @rolls = []
 
-    2.times do
-      die = rand(1..6)
-  
+    @num_dice.times do
+      die = rand(1..@sides)
+
       @rolls.push(die)
     end
 
-    render({:template => "dice/two_six"})
+    render({:template => "dice/flexible"})
   end
-
-  # ------------------------------------------------------------------ #
-  
-  def two_ten
-    @rolls = []
-
-    2.times do
-      die = rand(1..10)
-  
-      @rolls.push(die)
-    end
-
-    render({:template => "dice/two_ten"})
-  end
-
-  # ------------------------------------------------------------------ #
-
-  def one_twenty
-    @rolls = []
-
-    1.times do
-      die = rand(1..20)
-  
-      @rolls.push(die)
-    end
-
-    render({:template => "dice/one_twenty"})
-  end
-
-  # ------------------------------------------------------------------ #
-
-  def five_four
-    @rolls = []
-
-    5.times do
-      die = rand(1..4)
-  
-      @rolls.push(die)
-    end
-
-    render({:template => "dice/five_four"})
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "main", :action => "home" })
-  get("dice/2/6", { :controller => "main", :action => "two_six" })
-  get("dice/2/10", { :controller => "main", :action => "two_ten" })
-  get("dice/1/20", { :controller => "main", :action => "one_twenty" })
-  get("dice/5/4", { :controller => "main", :action => "five_four" })
+  get("dice/:number_of_dice/:how_many_sides", { :controller => "main", :action => "flexible" })
+  # get("dice/2/10", { :controller => "main", :action => "two_ten" })
+  # get("dice/1/20", { :controller => "main", :action => "one_twenty" })
+  # get("dice/5/4", { :controller => "main", :action => "five_four" })
 end
