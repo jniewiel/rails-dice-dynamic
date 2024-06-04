153329f335dcb17f2d783cc122972bce00ab3ef2
Jan Niewielski <39317379+jniewiel@users.noreply.github.com>
2024-06-04 19:27:09 +0000

    Created all dice views.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,8 @@ class MainController < ApplicationController
     render({:template => "dice/home"})
   end
 
+  # ------------------------------------------------------------------ #
+
   def two_six
     @rolls = []
 
@@ -14,6 +16,48 @@ class MainController < ApplicationController
     end
 
     render({:template => "dice/two_six"})
+  end
+
+  # ------------------------------------------------------------------ #
+  
+  def two_ten
+    @rolls = []
+
+    2.times do
+      die = rand(1..10)
+  
+      @rolls.push(die)
+    end
+
+    render({:template => "dice/two_ten"})
+  end
+
+  # ------------------------------------------------------------------ #
+
+  def one_twenty
+    @rolls = []
+
+    1.times do
+      die = rand(1..20)
+  
+      @rolls.push(die)
+    end
+
+    render({:template => "dice/one_twenty"})
+  end
+
+  # ------------------------------------------------------------------ #
+
+  def five_four
+    @rolls = []
+
+    5.times do
+      die = rand(1..4)
+  
+      @rolls.push(die)
+    end
+
+    render({:template => "dice/five_four"})
   end
 
 end
